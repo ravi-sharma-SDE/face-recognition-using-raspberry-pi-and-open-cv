271295fe36ba2d63f71f484113bb369fba7699ec
i-ravi <rash24680@gmail.com>
2024-04-30 18:59:24 +0530

    Added raspberry pi code

--- recognizer.py
+++ recognizer.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
 import os
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 relay = 23
-# GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(relay, GPIO.OUT)
-# GPIO.output(relay ,1)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(relay, GPIO.OUT)
+GPIO.output(relay ,1)
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('trainer.yml')
 cascadePath = "haarcascade_frontalface_default.xml"
@@ -27,7 +27,7 @@ minW = 0.1*cam.get(3)
 minH = 0.1*cam.get(4)
 while True:
     ret, img =cam.read()
-    # img = cv2.flip(img, -1) # Flip vertically
+    img = cv2.flip(img, -1) # Flip vertically
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale( 
         gray,
@@ -43,14 +43,14 @@ while True:
         if (confidence < 100):
             id = names[id]
             confidence = "  {0}%".format(round(100 - confidence))
-            # GPIO.output(relay, 0)
+            GPIO.output(relay, 0)
             print("Opening Lock")
-#            time.sleep(1)
- #           GPIO.output(relay, 1)
+            time.sleep(1)
+            GPIO.output(relay, 1)
         else:
             id = "unknown"
             confidence = "  {0}%".format(round(100 - confidence))
-            # GPIO.output(relay, 1)
+            GPIO.output(relay, 1)
         cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
         cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
     cv2.imshow('camera',img)
